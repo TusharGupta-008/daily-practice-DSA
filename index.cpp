6a1d450787d9f5eae8155f545acfa9a3b53b3d7c
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-08 20:00:14 +0530

    Reverse an Array

--- index.cpp
+++ index.cpp
@@ -1,15 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[] = {4, 9, 2, 7, 5};
+//     int n = 5;
+
+//     int maxVal = arr[0];
+//     for(int i = 1; i < n; i++) {
+//         if(arr[i] > maxVal)
+//             maxVal = arr[i];
+//     }
+
+//     cout << "Largest = " << maxVal;
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {4, 9, 2, 7, 5};
+    int arr[] = {1,2,3,4,5};
     int n = 5;
 
-    int maxVal = arr[0];
-    for(int i = 1; i < n; i++) {
-        if(arr[i] > maxVal)
-            maxVal = arr[i];
+    for(int i = 0; i < n/2; i++) {
+        swap(arr[i], arr[n-1-i]);
     }
 
-    cout << "Largest = " << maxVal;
+    for(int x : arr)
+        cout << x << " ";
 }
+
