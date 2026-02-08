b845c84ece76a836e4e0dba1e441c5f19140c7a0
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-08 20:01:13 +0530

    Count Even and Odd Numbers

--- index.cpp
+++ index.cpp
@@ -14,18 +14,37 @@
 //     cout << "Largest = " << maxVal;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[] = {1,2,3,4,5};
+//     int n = 5;
+
+//     for(int i = 0; i < n/2; i++) {
+//         swap(arr[i], arr[n-1-i]);
+//     }
+
+//     for(int x : arr)
+//         cout << x << " ";
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {1,2,3,4,5};
-    int n = 5;
+    int arr[] = {1,2,3,4,5,6};
+    int n = 6;
+    int even = 0, odd = 0;
 
-    for(int i = 0; i < n/2; i++) {
-        swap(arr[i], arr[n-1-i]);
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
     }
 
-    for(int x : arr)
-        cout << x << " ";
+    cout << "Even: " << even << " Odd: " << odd;
 }
+
 
