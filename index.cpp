d5a389c8c95d725382d9b8092a02be1c1872e134
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-12 23:21:12 +0530

    Reverse a String

--- index.cpp
+++ index.cpp
@@ -104,3 +104,14 @@
 
 //     cout << "Not found";
 // }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "hello";
+
+    for(int i = s.length()-1; i >= 0; i--) {
+        cout << s[i];
+    }
+}
