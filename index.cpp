386f70de491678c0e03e0639d33d73c1f513b336
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-16 15:19:22 +0530

    Count Frequency of Each Character

--- index.cpp
+++ index.cpp
@@ -178,3 +178,20 @@
 //         }
 //     }
 // }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "banana";
+    int freq[26] = {0};
+
+    for(char c : s) {
+        freq[c - 'a']++;
+    }
+
+    for(int i = 0; i < 26; i++) {
+        if(freq[i] > 0)
+            cout << char(i + 'a') << " : " << freq[i] << endl;
+    }
+}
