e9dbf5007b4cbfede371c9e72231b011b4e3399c
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-18 17:52:15 +0530

    new code

--- recursion.cpp
+++ recursion.cpp
@@ -12,16 +12,16 @@
 //     cout << fact(5);
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int fib(int n) {
-    if(n == 0) return 0;
-    if(n == 1) return 1;
+// int fib(int n) {
+//     if(n == 0) return 0;
+//     if(n == 1) return 1;
 
-    return fib(n-1) + fib(n-2);
-}
+//     return fib(n-1) + fib(n-2);
+// }
 
-int main() {
-    cout << fib(7);
-}
+// int main() {
+//     cout << fib(7);
+// }
