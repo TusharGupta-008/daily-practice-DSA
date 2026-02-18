3de8b9ebafff16ae37659caf59f443996ed22616
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-18 17:51:42 +0530

    Fibonacci Using Recursion

--- recursion.cpp
+++ recursion.cpp
@@ -11,3 +11,17 @@
 // int main() {
 //     cout << fact(5);
 // }
+
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    cout << fib(7);
+}
