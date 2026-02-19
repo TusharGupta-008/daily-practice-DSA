456231931d05527b8854f1c4473117cb30a10513
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-19 21:36:03 +0530

    new code

--- recursion.cpp
+++ recursion.cpp
@@ -54,15 +54,15 @@
 //     printRev(5);
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int sumN(int n) {
-    if(n == 0) return 0;
+// int sumN(int n) {
+//     if(n == 0) return 0;
 
-    return n + sumN(n-1);
-}
+//     return n + sumN(n-1);
+// }
 
-int main() {
-    cout << sumN(5);
-}
+// int main() {
+//     cout << sumN(5);
+// }
