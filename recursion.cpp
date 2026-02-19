5da06b0ca4abce8825e0550f8f1e1b116717e72e
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-19 21:35:33 +0530

    Sum of First N Numbers

--- recursion.cpp
+++ recursion.cpp
@@ -53,3 +53,16 @@
 // int main() {
 //     printRev(5);
 // }
+
+#include <iostream>
+using namespace std;
+
+int sumN(int n) {
+    if(n == 0) return 0;
+
+    return n + sumN(n-1);
+}
+
+int main() {
+    cout << sumN(5);
+}
