0586eecaf11124ac1c3ab991969a3a0369f851e9
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-19 21:33:11 +0530

    Print Numbers 1 to N

--- recursion.cpp
+++ recursion.cpp
@@ -25,3 +25,17 @@
 // int main() {
 //     cout << fib(7);
 // }
+
+#include <iostream>
+using namespace std;
+
+void printN(int n) {
+    if(n == 0) return;
+
+    printN(n-1);
+    cout << n << " ";
+}
+
+int main() {
+    printN(5);
+}
