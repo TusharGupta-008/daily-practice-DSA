34d916e84063eb7381b0571ffb73b88d762ecd95
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-22 22:28:21 +0530

    Power of Number (a^b)

--- recursion.cpp
+++ recursion.cpp
@@ -128,3 +128,16 @@
 //     string s = "madam";
 //     cout << isPal(s, 0, s.length()-1);
 // }
+
+#include <iostream>
+using namespace std;
+
+int power(int a, int b) {
+    if(b == 0) return 1;
+
+    return a * power(a, b-1);
+}
+
+int main() {
+    cout << power(2, 5);
+}
