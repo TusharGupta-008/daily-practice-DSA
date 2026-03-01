71ea01c293ac1f09f465a67a73fddad87efc466f
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-03-01 22:08:46 +0530

    Print Even Numbers from 1 to N (Recursion)

--- recursion.cpp
+++ recursion.cpp
@@ -196,3 +196,20 @@
 //     int arr[] = {1,2,3,4,5};
 //     cout << sumArray(arr,5);
 // }
+
+#include <iostream>
+using namespace std;
+
+void printEven(int n) {
+    if(n == 0)
+        return;
+
+    printEven(n-1);
+
+    if(n % 2 == 0)
+        cout << n << " ";
+}
+
+int main() {
+    printEven(10);
+}
