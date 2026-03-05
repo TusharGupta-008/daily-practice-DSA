547c6281f18d4a46c2f3e0a034974e4af307f9cd
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-03-05 20:09:09 +0530

    new code

--- DSA/basic.cpp
+++ DSA/basic.cpp
@@ -44,18 +44,18 @@ int main()
 //     }
 // };
 
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        int nonZero = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[nonZero]);
-                nonZero++;
-            }
-        }
-    }
-};
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int nonZero = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] != 0) {
+//                 swap(nums[i], nums[nonZero]);
+//                 nonZero++;
+//             }
+//         }
+//     }
+// };
 
  
 }
