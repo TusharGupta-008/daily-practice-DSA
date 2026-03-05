24ed546253c1c0e8c7a2cc407f4cb77806eeff9b
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-03-05 20:00:57 +0530

    new pratice file added

---
+++ DSA/basic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  // // cout<<"Enter the number: ";
+  // int n;
+  // cin>>n;
+  // // cout<<"The numbers are ";
+  // for(int i=1;i<=n;i++){
+  //   cout<<i<<" ";
+  // }
+  // int n[5]={1,2,3,4,5};
+  // cout<<n[4];
+
+  string str = "Hello";
+
+  cout << str;
+}
