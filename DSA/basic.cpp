305511cf6ec6eb386f99f8e14540771e372382fd
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-03-05 20:08:43 +0530

    move zeroes

--- DSA/basic.cpp
+++ DSA/basic.cpp
@@ -44,5 +44,18 @@ int main()
 //     }
 // };
 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonZero = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[nonZero]);
+                nonZero++;
+            }
+        }
+    }
+};
+
  
 }
