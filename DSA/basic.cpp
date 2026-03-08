c6393c7d5a30aa0b6566ae3fe10559cae093fe6b
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-03-08 23:35:38 +0530

    GCD

--- DSA/basic.cpp
+++ DSA/basic.cpp
@@ -75,3 +75,17 @@ int main()
 
 
 }
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+int main() {
+    cout << gcd(12,18);
+}
